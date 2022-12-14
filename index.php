fffd2ba1359e0f20b2ba313aa8c854c6ceb5afa2
KINGRNR <rafifnr06@gmail.com>
2022-12-14 10:46:36 +0700

    menambah styling

--- index.php
+++ index.php
@@ -9,15 +9,14 @@ $data = mysqli_query($conn, "SELECT * FROM tb_siswa");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <title>Data Siswa</title>
 </head>
 
-<body>
+<body style="padding: 100px;">
 
-    <table border="1" align="center" cellpadding="10px">
-        <tr>
-            <th><a href="tampilan/insert.php">Tambah Data</a></th>
-        </tr>
+    <table class="table table-striped" style="text-align: center;">
         <tr>
             <th>ID</th>
             <th>Nama</th>
@@ -38,9 +37,12 @@ $data = mysqli_query($conn, "SELECT * FROM tb_siswa");
                     ?></td>
                 <td><?= $row['alamat'] ?></td>
                 <td><a href="tampilan/update.php?id=<?php echo $row['id']; ?>">Update</a></td>
-                <td><a href="fungsi/fungsi_delete.php?id=<?php echo $row['id']; ?>" onclick="return confirm('apakah anda yakin?')">Hapus</a></td>
+                <td><a href="fungsi/fungsi_delete.php?id=<?php echo $row['id']; ?>" onclick="return confirm('apakah anda yakin?')" style="color: red;">Hapus</a></td>
             </tr>
         <?php endwhile; ?>
+        <tr>
+            <th colspan="6"><a href="tampilan/insert.php">Tambah Data</a></th>
+        </tr>
     </table>
 </body>
 
--- tampilan/insert.php
+++ tampilan/insert.php
@@ -5,13 +5,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <title>Tambah Data</title>
+    <style>
+        h1 {
+            text-align: center;
+        }
+
+        table tr td {
+            padding: 10px;
+        }
+    </style>
 </head>
 
 <body>
     <h1>Tambah Data Siswa</h1>
     <form action="../fungsi/fungsi_insert.php" method="post">
-        <table>
+        <table align="center">
             <tr>
                 <td>Nama Siswa</td>
                 <td>:</td>
